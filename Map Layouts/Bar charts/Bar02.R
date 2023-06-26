2f580fa9e0b805346cd77f8911ef40c61fb8d87f
lbreeding <127157740+lbreeding@users.noreply.github.com>
2023-06-26 15:54:55 -0700

    Update Bar02.R

--- Map Layouts/Bar charts/Bar02.R
+++ Map Layouts/Bar charts/Bar02.R
@@ -1,10 +1,9 @@
-# Create a data frame with the given data
 data <- data.frame(
   Category = 1:4,
-  Value1 = c(15.935, 60.629, 70.573, 78.404),
-  Value2 = c(27.441, 57.617, 69.072, 78.518),
-  Value3 = c(18.414, 52.808, 64.429, 72.531),
-  Value4 = c(33.867, 64.223, 75.221, 84.231)
+  Value1 = c(20.854, 62.816, 75.306, 83.21),
+  Value2 = c(39.673, 65.373, 76.773, 84.42),
+  Value3 = c(15.935, 60.629, 70.573, 78.404),
+  Value4 = c(27.441, 57.617, 69.072, 78.518)
 )
 
 # Load the necessary library
@@ -16,18 +15,18 @@ data_long <- tidyr::gather(data, Key, Value, -Category)
 # Create the bar chart
 chart <- ggplot(data_long, aes(x = as.factor(Category), y = Value, fill = Key)) +
   geom_bar(stat = "identity", position = position_dodge(width = 0.7), width = 0.6, color = "black") +
-  geom_segment(aes(x = 3.6, xend = 4.4, y = 38, yend = 38),
+  geom_segment(aes(x = 3.6, xend = 4.4, y = 83.453, yend = 83.453),
                linetype = "dashed", color = "black") +
-  geom_segment(aes(x = 0.6, xend = 1.4, y = 54.997, yend = 54.997),
+  geom_segment(aes(x = 0.6, xend = 1.4, y = 38.261, yend = 38.261),
                linetype = "dashed", color = "black") +
-  geom_segment(aes(x = 1.6, xend = 2.4, y = 69.634, yend = 69.634),
+  geom_segment(aes(x = 1.6, xend = 2.4, y = 54.997, yend = 54.997),
                linetype = "dashed", color = "black") +
-  geom_segment(aes(x = 2.6, xend = 3.4, y = 83.453, yend = 83.453),
+  geom_segment(aes(x = 2.6, xend = 3.4, y = 69.634, yend = 69.634),
                linetype = "dashed", color = "black") +
-  geom_text(aes(x = 1, y = 46, label = "Observed"),
+  geom_text(aes(x = 1, y = 40, label = "Observed"),
             color = "black", size = 3, vjust = -0.5) +
   
- 
+  
   labs(title = "", x = "Running Mean (yr)", y = "% of Normal") +
   scale_fill_manual(
     values = c("Value1" = "black", "Value2" = "grey", "Value3" = "blue2", "Value4" = "cyan3"),
@@ -49,7 +48,4 @@ chart <- ggplot(data_long, aes(x = as.factor(Category), y = Value, fill = Key)) 
 ggsave("bar_chart05.png", plot = chart + guides(fill = guide_legend(nrow = 4, byrow = TRUE, keyheight = unit(.7, "cm"))) +
          theme(legend.text = element_text(size = 13)), 
        width = 8, height = 6, dpi = 300, bg = "white")
-
-
-
 
